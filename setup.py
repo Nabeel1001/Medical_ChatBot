ff12586e28d05cef1a2d990893e0f642e83091a0
Nabeel1001 <nabeelk_1001@hotmail.com>
2026-01-28 20:26:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nabeel Abdul Aziz Khan",
+    author_email="nabeelk_1001@hotmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
